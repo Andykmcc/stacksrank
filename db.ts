448d9e75d53c5f379e5aface5828911b21e01242
Andy McCoy <andy.k.mccoy@gmail.com>
2024-11-27 12:50:43 +0000

    remove unused code

--- db.ts
+++ db.ts
@@ -1,10 +1,8 @@
 // db.js
 import Dexie, { type EntityTable } from 'dexie';
-import { boolean, z } from 'zod';
+import { z } from 'zod';
 
-Dexie.debug = true;
-
-export const uuid = z.custom<`${string}-${string}-${string}-${string}-${string}`>(val => 
+export const uuid = z.custom<`${string}-${string}-${string}-${string}-${string}`>(val =>
   typeof val === "string" ? /^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12}$/.test(val) : false
 ).default("00000000-0000-4000-9800-000000000000");
 export type Uuid = z.infer<typeof uuid>; // `${string}-${string}-${string}-${string}-${string}`
@@ -34,12 +32,6 @@ export const stackSchema = z.object({
 });
 export type Stack = z.output<typeof stackSchema>;
 
-export const shortcutSchema = z.object({
-  key: z.string(),
-  value: z.any(),
-});
-export type Shortcut = z.output<typeof shortcutSchema>
-
 export const db = new Dexie('StacksRank') as Dexie & {
   users: EntityTable<
     User,
@@ -49,19 +41,9 @@ export const db = new Dexie('StacksRank') as Dexie & {
     Stack,
     'id'
   >;
-  works: EntityTable<
-    Work,
-    'id'
-  >;
-  shortcuts: EntityTable<
-    Shortcut,
-    'key'
-  >;
 };
 
 db.version(1).stores({
   users: 'id',
   stacks: 'id,user_id',
-  works: 'id,title,author_name',
-  shortcuts: 'key',
 });
