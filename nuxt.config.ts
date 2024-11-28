47fde36c26fe1b81c2a0a649431741cf1322b2dd
Andy McCoy <andy.k.mccoy@gmail.com>
2024-11-28 02:20:04 +0000

    disable zoom on mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover',
+      title: 'Stacks Rank'
+    }
+  },
   ssr: false,
   nitro: {
     prerender: {
